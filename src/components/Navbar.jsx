b32902f86bf3c1abfe13effd47bdb639c51634d0
tunny17 <tvnji17@gmail.com>
2023-03-01 20:28:00 +0100

    addition of the router tag to th index.js file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <div className="logo-container">
             <Avatar src={icon} size='large' />
             <Typography.Title level={2} className='logo'>
-                <link  to='/'>CryptoVerse</link>
+                <Link  to='/'>CryptoVerse</Link>
             </Typography.Title>
             {/* <Button className='menu-control-container'>
 
