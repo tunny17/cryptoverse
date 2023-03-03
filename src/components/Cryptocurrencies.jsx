01e30f0f6d5ff471aeca2199a7ffd6084c8913c6
tunny17 <tvnji17@gmail.com>
2023-03-03 21:01:46 +0100

    bug fix (for cases when the value is undefined)

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -14,10 +14,12 @@ const Cryptocurrencies = ({ simplified }) => {
 
   console.log(cryptos);
 
+  if(isFetching) return 'Loading...';
+
   return (
     <>
       <Row  gutter={[32, 32]}  className='crypto-card-container'>
-        {cryptos.map((currency) => (
+        {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
               <Card 
